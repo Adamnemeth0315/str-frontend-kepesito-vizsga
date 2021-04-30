518c3a88201f9b7893d583cda99a98eff2c0998a
Adamnemeth0315 <adam.nemeth.0315@gmail.com>
2021-04-30 12:13:42 +0200

    js DOm félig kész

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,7 +1,19 @@
 function changeOuterLinks() {
     const links = document.getElementsByTagName('a');
-    links.forEach(item => item.innerHTML.includes('outer-link').setAttribute('target', '_blank'));
-}
+     links.forEach(item => {
+        if (item.innerHTML.includes('outer-link')) {
+            item.setAttribute('target', '_blank')
+        };
+    }
+    )} 
+    const nav = document.querySelector('#link-list');
+    nav.style.display = "flex";
+    nav.style.flexDirection = "column";
+    nav.style.width = "30%";
+    nav.style.margin = "0px auto";
+    nav.style.border = "1px solid blue";
+    nav.style.padding = "16px";
+    }
 
 
 export { changeOuterLinks };
